2e5d9b3710ef311e9073520775bf405bfa49df20
Raggi <raggi@pharous.com>
2021-02-02 16:22:06 +0200

    add Response Helpers & ExceptionHandler

---
+++ src/Classes/ExceptionHandler.php
@@ -0,0 +1,74 @@
+<?php
+namespace Pharaonic\Laravel\Helpers\Classes;
+
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Foundation\Exceptions\Handler;
+use Illuminate\Validation\ValidationException;
+use Throwable;
+
+class ExceptionHandler extends Handler
+{
+    /**
+     * A list of the exception types that are not reported.
+     *
+     * @var array
+     */
+    protected $dontReport = [
+        //
+    ];
+
+    /**
+     * A list of the inputs that are never flashed for validation exceptions.
+     *
+     * @var array
+     */
+    protected $dontFlash = [
+        'password',
+        'password_confirmation',
+    ];
+
+    /**
+     * Render an exception into an HTTP response.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Throwable  $exception
+     * @return \Symfony\Component\HttpFoundation\Response
+     *
+     * @throws \Throwable
+     */
+    public function render($request, Throwable $exception)
+    {
+        if ($request->expectsJson() && get_class($exception) != AuthenticationException::class) return json(false, $exception->getMessage(), (object) [
+            'line'  => $exception->getLine(),
+            'file'  => $exception->getFile()
+        ]);
+
+        return parent::render($request, $exception);
+    }
+
+    /**
+     * Convert a validation exception into a JSON response.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Validation\ValidationException  $exception
+     * @return \Illuminate\Http\JsonResponse
+     */
+    protected function invalidJson($request, ValidationException $exception)
+    {
+        return json(false, $exception->getMessage(), $exception);
+    }
+
+    /**
+     * Convert an authentication exception into a response.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Auth\AuthenticationException  $exception
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    protected function unauthenticated($request, AuthenticationException $exception)
+    {
+        return $request->expectsJson()
+            ? json(false, 'unauthenticated', null, null, 401)
+            : redirect()->guest($exception->redirectTo() ?? route('login'));
+    }
+}
---
+++ src/Helpers.php
@@ -0,0 +1,71 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+/**
+ * Create a new JSON response instance
+ *
+ * @param boolean $success
+ * @param null|string $message
+ * @param null|string|array|object $data
+ * @param null|array $errors
+ * @param integer $status
+ * @param array $headers
+ * @param integer $options
+ * @return \Illuminate\Http\JsonResponse
+ */
+function json(bool $success, string $message = null, $data = null, array $errors = null, $status = 200, array $headers = null, $options = 0)
+{
+    // Casting Errors to Array of Objects
+    if ($errors) {
+        $errList = [];
+        foreach ($errors as $key => $err) {
+            $errList[] = (object)[
+                'key'   => $key,
+                'value' => implode(PHP_EOL, $err)
+            ];
+        }
+        $errors = $errList;
+        unset($errList);
+    }
+
+    // Response
+    return response()->json([
+        'success'   => $success,
+        'message'   => $message,
+        'errors'    => $errors,
+        'data'      => $data
+    ], $status, $headers ?? [], $options);
+}
+
+/**
+ * Request Inputs Validation
+ *
+ * @param Request $request
+ * @param array $rules
+ * @param null||string $defaultMessage
+ * @param null||array $messages
+ * @param null||array $fields
+ * @return null||\Illuminate\Http\JsonResponse
+ */
+function validate(Request $request, array $rules, string $defaultMessage = null, array $messages = null, array $fields = null, bool $redirectToRoute = false, string $redirectTo = null, array $redirectParams = null)
+{
+    $validator = Validator::make($request->only(array_keys($rules)), $rules, $messages ?? [], $fields ?? []);
+
+    if ($validator->fails()) {
+        if ($request->expectsJson()) {
+            // JSON Request
+            return json(false, $defaultMessage, null, $validator->errors()->toArray());
+        } else {
+            // Web Request
+            if ($redirectToRoute) {
+                return redirect()->route($redirectTo, $redirectParams)->withErrors($validator)->withInput();
+            } else {
+                return redirect()->to($redirectTo)->withErrors($validator)->withInput();
+            };
+        }
+    }
+
+    return null;
+}
