ca103af33fc2d910b1ce94e2332fcb0cf64e3403
Moamen Eltouny <16774083+RaggiTech@users.noreply.github.com>
2021-12-25 09:53:06 +0200

    remove json function
    - move json function to Jsonable Package

--- src/Helpers.php
+++ src/Helpers.php
@@ -4,43 +4,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
 /**
- * Create a new JSON response instance
- *
- * @param boolean $success
- * @param null|string $message
- * @param null|string|array|object $data
- * @param null|array $extra
- * @param null|array $errors
- * @param integer $status
- * @param array $headers
- * @param integer $options
- * @return \Illuminate\Http\JsonResponse
- */
-function json(bool $success, string $message = null, $data = null, array $extra = null, array $errors = null, $status = 200, array $headers = null, $options = 0)
-{
-    // Casting Errors to Array of Objects
-    if ($errors) {
-        $errList = [];
-        foreach ($errors as $key => $err) {
-            $errList[] = (object)[
-                'key'   => $key,
-                'value' => implode(PHP_EOL, $err)
-            ];
-        }
-        $errors = $errList;
-        unset($errList);
-    }
-
-    // Response
-    return response()->json([
-        'success'   => $success,
-        'message'   => $message,
-        'errors'    => $errors,
-        'data'      => $data
-    ] + ($extra ?? []), $status, $headers ?? [], $options);
-}
-
-/**
  * Request Inputs Validation
  *
  * @param Request $request
